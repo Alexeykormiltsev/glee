28135ea71d31686d95776f41730bc5c2e13c0ce1
Alexeykormiltsev <cormiltsev@yandex.ru>
2022-05-20 17:30:41 +0500

    added product-list1 page

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,24 @@ $(function(){
         autoplaySpeed: 5000
        });
 });
+
+$('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+  },
+  });
+  $(".filter-product__star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+    spacing: "5px"
+  });
+ 
